ffb19f43d894eb6a12d47e330150f6533793ddd4
Ch4nYH <hfercxx@hotmail.com>
2020-10-31 12:03:47 +0800

    update

--- networks/networks.py
+++ networks/networks.py
@@ -137,44 +137,37 @@ class BottleNeck(nn.Module):
     def forward(self, x):
         return nn.ReLU(inplace=True)(self.residual_function(x) + self.shortcut(x))
 
-class ResNet18(utils.ReparamModule):
-    supported_dims = {28, 32}
-
-    class BasicBlock(nn.Module):
-        """Basic Block for resnet 18 and resnet 34
-        """
-
-        #BasicBlock and BottleNeck block
-        #have different output size
-        #we use class attribute expansion
-        #to distinct
-        expansion = 1
-
-        def __init__(self, state, in_channels, out_channels, stride=1):
-            super().__init__()
-
-            #residual function
-            self.residual_function = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
-                nn.BatchNorm2d(out_channels),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(out_channels, out_channels * BasicBlock.expansion, kernel_size=3, padding=1, bias=False),
+class BasicBlock(utils.ReparamModule):
+    """Basic Block for resnet 18 and resnet 34
+    """
+    #BasicBlock and BottleNeck block
+    #have different output size
+    #we use class attribute expansion
+    #to distinct
+    expansion = 1
+    def __init__(self, state, in_channels, out_channels, stride=1):
+        super().__init__()
+        #residual function
+        self.residual_function = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels * BasicBlock.expansion, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels * BasicBlock.expansion)
+        )
+        #shortcut
+        self.shortcut = nn.Sequential()
+        #the shortcut output dimension is not the same with residual function
+        #use 1*1 convolution to match the dimension
+        if stride != 1 or in_channels != BasicBlock.expansion * out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels * BasicBlock.expansion, kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(out_channels * BasicBlock.expansion)
             )
-
-            #shortcut
-            self.shortcut = nn.Sequential()
-
-            #the shortcut output dimension is not the same with residual function
-            #use 1*1 convolution to match the dimension
-            if stride != 1 or in_channels != BasicBlock.expansion * out_channels:
-                self.shortcut = nn.Sequential(
-                    nn.Conv2d(in_channels, out_channels * BasicBlock.expansion, kernel_size=1, stride=stride, bias=False),
-                    nn.BatchNorm2d(out_channels * BasicBlock.expansion)
-                )
-
-        def forward(self, x):
-            return nn.ReLU(inplace=True)(self.residual_function(x) + self.shortcut(x))
+    def forward(self, x):
+        return nn.ReLU(inplace=True)(self.residual_function(x) + self.shortcut(x))
+class ResNet18(utils.ReparamModule):
+    supported_dims = {28, 32}
 
     def __init__(self, state, block = BasicBlock, num_block=[2, 2, 2, 2]):
         super().__init__()
